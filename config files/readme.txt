d0636c93a5b2c1e0651d0e0845e418bc9b614105
MichaelJarratt <mike.jarratt@virginmedia.com>
2020-09-28 10:11:46 +0100

    Untracked config files, added gitignore to not track them in futue. Added cdefault-config -example.json which explains what each field in the config file is for and does not have a login token

--- config files/readme.txt
+++ config files/readme.txt
@@ -1,1 +1,3 @@
-Each server the bot is a part of will have an entry here with personalised settings
+Each server the bot is a part of will have an entry here with personalised settings.
+When the bot joins a fresh server it will use the default configuration, when it is set up on that server the configuration 
+will be saved to a file with the name of that servers ID
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*  This file servers no purpose for bot function.
 *   It opens an express webserver so that the bot will be kept alive on reple.it as outlined in the link below
-*/  https://repl.it/talk/learn/Hosting-discordjs-bots-on-replit-Works-for-both-discordjs-and-Eris/11027
+*/  //https://repl.it/talk/learn/Hosting-discordjs-bots-on-replit-Works-for-both-discordjs-and-Eris/11027
 
 const express = require('express');
 const server = express();
